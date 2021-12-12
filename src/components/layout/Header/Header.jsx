f9592ed4efe1d1b720efb8e5556b060067260876
Vijendra-Achar <vijendra.1995@gmail.com>
2021-12-12 18:58:42 +0530

    Header styling in progress 🔃

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 
 import { FaSun, FaPlusCircle } from 'react-icons/fa';
 
+import './Header.scss';
+// import './../../../scss/_common.scss';
+
+import TodoistLogo from './../../../assets/todoist_logo.svg';
+
 export const Header = () => {
   return (
     <header className="header" data-testid="header">
-      <nav>
-        <div className="logo">
-          <img src="./images/logo.png" alt="Todoist" />
+      <nav className="container">
+        <div>
+          <img className="header__logo" src={TodoistLogo} alt="Todoist" />
         </div>
 
-        <div className="settings">
+        <div className="header__settings">
           <ul>
             <li>
               <FaPlusCircle />

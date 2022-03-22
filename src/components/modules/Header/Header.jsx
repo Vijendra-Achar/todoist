928512fab0c7a686ee2c9533211850b983bd4e22
Vijendra-Achar <vijendra.1995@gmail.com>
2022-03-22 22:50:05 +0530

    Header styling in progress

--- src/components/modules/Header/Header.jsx
+++ src/components/modules/Header/Header.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 
+import TodoistLogo from "../../../assets/icons/todoist_logo.svg";
+
+import { MdMenu } from "react-icons/md";
+
+import "./Header.scss";
+
 const Header = () => {
   return (
     <div className="header">
-      <div className="header__left-side">
-        <div className="header__hamburger-menu"></div>
-        <div className="header__logo"></div>
-        <div className="header__search-bar"></div>
-      </div>
+      <div className="header__container">
+        <div className="header__left-side">
+          <div className="header__logo">
+            <img src={TodoistLogo} alt="todoist_logo" />
+          </div>
+          <div className="header__search-bar"></div>
+        </div>
 
-      <div className="header__right-side">
-        <div className="header__add-task"></div>
-        <div className="header__help"></div>
-        <div className="header__authentication">
-          <div className="header__authentication__user-avatar"></div>
-          <div className="header__authentication__buttons">
-            <div className="header__authentication__button">Login</div>
-            <div className="header__authentication__button">Sign up</div>
+        <div className="header__right-side">
+          <div className="header__add-task"></div>
+          <div className="header__help"></div>
+          <div className="header__authentication">
+            <div className="header__authentication__user-avatar"></div>
+            <div className="header__authentication__buttons">
+              <div className="header__authentication__button">Login</div>
+              <div className="header__authentication__button">Sign up</div>
+            </div>
           </div>
         </div>
       </div>

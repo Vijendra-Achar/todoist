bfe059a305356a686e8fff80637a322c03f4115e
Vijendra-Achar <vijendra.1995@gmail.com>
2022-04-06 23:01:05 +0530

    React router dom - installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/modules/Header/Header.jsx";
+import Login from "./components/pages/Login/Login.jsx";
+import Home from "./components/pages/Home/Home.jsx";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 };

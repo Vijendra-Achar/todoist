e70738467536f7600cebb6fae9a8fc85608ea51e
Vijendra-Achar <vijendra.1995@gmail.com>
2022-05-12 07:02:14 +0530

    Sidebar structure complete

--- src/components/modules/Sidebar/Sidebar.jsx
+++ src/components/modules/Sidebar/Sidebar.jsx
@@ -3,7 +3,13 @@ import React from "react";
 import "./Sidebar.scss";
 
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebat__section"></div>
+      <div className="sidebat__section"></div>
+      <div className="sidebat__section"></div>
+    </div>
+  );
 };
 
 export default Sidebar;

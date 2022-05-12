d208dcfd434dffc1fecdf5424d19fd4f7305091e
Vijendra-Achar <vijendra.1995@gmail.com>
2022-05-12 07:50:12 +0530

    Sidebar implementation in-progress ⌛️

--- src/components/modules/Sidebar/Sidebar.jsx
+++ src/components/modules/Sidebar/Sidebar.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 
+import { BsFillCalendar2EventFill, BsFillCalendarRangeFill } from "react-icons/bs";
+
 import "./Sidebar.scss";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="sidebat__section"></div>
-      <div className="sidebat__section"></div>
-      <div className="sidebat__section"></div>
+      <div className="sidebar__section">
+        <div className="sidebar__section__menu-item">
+          <BsFillCalendar2EventFill />
+          <p className="sidebar__section__menu-item__title">Today</p>
+        </div>
+        <div className="sidebar__section__menu-item">
+          <BsFillCalendarRangeFill />
+          <p className="sidebar__section__menu-item__title">Upcoming</p>
+        </div>
+      </div>
+      <div className="sidebar__section"></div>
+      <div className="sidebar__section"></div>
     </div>
   );
 };
